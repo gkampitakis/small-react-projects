63a7cbad92e6a9ac866fa1b3b88c13d035d5dcd2
gkampitakis <gkabitakis@gmail.com>
2020-10-31 09:37:20 +0000

    Start initial structure for creating projects

---
+++ src/projects/tours/tours.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default function birthday () {
+  return (
+    <div className="app">
+      <span>Hello World</span>
+    </div>
+  )
+}
