b5ab56f01999c4903723e5e6e9d264371636dbd8
gkampitakis <gkabitakis@gmail.com>
2020-11-02 23:41:35 +0000

    minor ui css changes for 2nd project

--- src/projects/tours/components/Tour.tsx
+++ src/projects/tours/components/Tour.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TourI } from './interface';
 
 export default function Tour ({ data, removeTour }: { data: TourI; removeTour: () => void }) {
+  const [more, setMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={data.image} alt={data.name} />
       <footer>
         <div className="tour-info">
-          <h2>{data.name}</h2>
-          <h3>{data.info}</h3>
-          <h3>{data.price}</h3>
+          <h4>{data.name}</h4>
+          <h4 className="tour-price">{data.price}</h4>
         </div>
+        <p>
+          {more ? data.info : `${data.info.substring(0, 200)}...`}
+          <button onClick={() => setMore(!more)}>{more ? 'show less' : 'read more'}</button>
+        </p>
         <button className="delete-btn" onClick={removeTour}>Not interested</button>
       </footer>
     </article>
