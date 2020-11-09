06585786b47cb136a9ab7569311a0f946923519f
gkampitakis <gkabitakis@gmail.com>
2020-11-09 19:15:17 +0000

    implement slider project

---
+++ src/projects/slider/Slider.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import people from './data';
+import Review from './components/Review';
+import './index.css';
+
+export default function Slider () {
+  const [index, setIndex] = useState(0);
+
+  function next () {
+    if (index + 1 >= people.length) {
+      return setIndex(0);
+    }
+    setIndex(i => i + 1);
+  }
+
+  function previous () {
+    if (index - 1 < 0) {
+      return setIndex(people.length - 1);
+    }
+    setIndex(i => i - 1);
+  }
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {
+          people.map((data, idx) => {
+
+            const { id, ...rest } = data;
+
+            let position = 'nextSlide';
+            if (idx === index) {
+              position = 'activeSlide';
+            }
+            if (
+              idx === index - 1 ||
+              (index === 0 && idx === people.length - 1)
+            ) {
+              position = 'lastSlide';
+            }
+
+            return (
+              <Review key={data.id} data={rest} className={position} />
+            )
+          })
+        }
+        <button className="prev" onClick={previous}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={next}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  )
+}
---
+++ src/projects/slider/components/Review.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import { FaQuoteRight } from 'react-icons/fa';
+
+interface ReviewProps {
+  className: string;
+  data: {
+    image: string;
+    name: string;
+    title: string;
+    quote: string;
+  };
+}
+
+export default function Review ({ data, className }: ReviewProps) {
+  return (
+    <article className={className}>
+      <img className="person-img" src={data.image} alt={data.name} />
+      <h4>{data.name}</h4>
+      <p className="title">{data.title}</p>
+      <p className="quote">{data.quote}</p>
+      <FaQuoteRight className="icon" />
+    </article>
+  )
+}
