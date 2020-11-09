b4d86eb692c871fa7485c4ac10eba7595173846f
gkampitakis <gkabitakis@gmail.com>
2020-11-09 20:02:33 +0000

    implement lorem ipsum generator

---
+++ src/projects/lorem_ipsum/LoremGenerator.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import loremData from './data';
+import './index.css';
+
+export default function LoremGenerator () {
+  const [count, setCount] = useState('0');
+  const [data, setData] = useState<string[]>([]);
+
+  function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+    const countInt = parseInt(count);
+    e.preventDefault();
+
+    if (countInt < 0) {
+      return alert('Please provide correct input');
+    }
+
+    if (countInt > loremData.length)
+      return setData(loremData.slice(0, loremData.length));
+
+    setData(loremData.slice(0, countInt));
+  }
+
+  return (
+    <section className="section-center">
+      <h3>Tired of borin Lorem Ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs: </label>
+        <input
+          type="number"
+          name="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {
+          data.map((item, idx) =>
+            (
+              <p key={idx}>{item}</p>
+            )
+          )
+        }
+      </article>
+    </section>
+  )
+}
