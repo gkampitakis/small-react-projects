ae3fe7027cfb59d22104507fc494b2a617e3e265
gkampitakis <gkabitakis@gmail.com>
2020-12-04 19:08:09 +0000

    fix: issue with images on homepage not being clickable

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 import { useBodyStyles } from '../hooks/useBodyStyles';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import ImageLoader from './ImageLoader';
 import routes from '../routes';
 import '../app.scss';
@@ -11,6 +11,8 @@ export default function Home (): ReactElement {
     background: 'hsl(210, 36%, 96%)',
     color: 'hsl(209, 61%, 16%)'
   });
+  const history = useHistory();
+
   return (
     <main className="homePage">
       <header>
@@ -19,7 +21,7 @@ export default function Home (): ReactElement {
       <section>
         {routes.map(({ projectName, route, imgPath }, idx) => (
           <div className="project" key={idx}>
-            <ImageLoader imgPath={imgPath} projectName={projectName} />
+            <ImageLoader onClick={() => history.push(route)} imgPath={imgPath} projectName={projectName} />
             <Link to={route}>
               {projectName}
             </Link>
--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -4,11 +4,12 @@ import React, { ReactElement } from 'react';
 interface ImageLoaderProps {
   imgPath: string;
   projectName: string;
+  onClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
 }
 
-export default function ImageLoader ({ imgPath, projectName }: ImageLoaderProps): ReactElement {
+export default function ImageLoader ({ imgPath, projectName, onClick }: ImageLoaderProps): ReactElement {
   return (
-    <picture>
+    <picture onClick={onClick}>
       <source srcSet={require(`${imgPath}.webp`).default} />
       <img src={require(`${imgPath}.png`).default} alt={`${projectName} Project`} />
     </picture>
